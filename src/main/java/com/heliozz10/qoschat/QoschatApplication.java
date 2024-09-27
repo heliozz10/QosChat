87273a4f1fea4d294bf93d0b0313329f736da64f
heliozz10 <helioa31415@gmail.com>
2024-09-27 19:57:24 +0500

    First commit

---
+++ src/main/java/com/heliozz10/qoschat/QoschatApplication.java
@@ -0,0 +1,13 @@
+package com.heliozz10.qoschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QoschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QoschatApplication.class, args);
+	}
+
+}
